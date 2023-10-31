1696e8ac966fddacacba2d29758847449933e5f7
Laurasouusa <laura.sousa@estudante.ifgoiano.edu.br>
2023-10-31 20:52:08 -0300

    alterações do Isaac

--- lib/view/pages/register_page.dart
+++ lib/view/pages/register_page.dart
@@ -16,102 +16,111 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Container(
-          width: 324,
-          height: 490,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Color(0xFFEA3026),
-              width: 1,
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //Ícone do Sistema
-              Container(
-                width: 158,
-                height: 158,
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              //Nome do Aplicativo
-              const Text(
-                'CADASTRO',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(height: 15),
-              //Input do nome
-              Container(
-                width: 270,
-                child: MyTextField(
-                  hintText: 'Nome',
-                  obscureText: false,
-                  controller: controllerUserName,
-                ),
-              ),
-              const SizedBox(height: 10),
-              //Input do email
-              Container(
-                width: 270,
-                child: MyTextField(
-                  hintText: 'Telefone',
-                  obscureText: false,
-                  controller: controllerEmail,
-                ),
-              ),
-              const SizedBox(height: 10),
-              //Input da senha
-              Container(
-                width: 270,
-                child: MyTextField(
-                  hintText: 'E-mail',
-                  obscureText: true,
-                  controller: controllerPass,
-                ),
-              ),
-              const SizedBox(height: 10),
-              //Input da confirme a senha
-              Container(
-                width: 270,
-                child: MyTextField(
-                  hintText: 'Senha',
-                  obscureText: true,
-                  controller: controllerConfirmPass,
-                ),
-              ),
-              const SizedBox(height: 15),
-              //Botão de login
-              MyButton(
-                buttonText: 'SALVAR',
-                onTapButton: () {
-                  displayMessage('Registrado', context);
-                },
-              ),
-              const SizedBox(height: 20),
-              //Possui uma conta? Faça login
-              Row(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(45.0),
+            child: SingleChildScrollView(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      goToLogin(context);
-                    },
-                    child: Text(
-                      'Possui uma conta? Faça login',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary,
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0xFFEA3026),
+                        width: 1,
                       ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        //Ícone do Sistema
+                        ClipRRect(
+                          child: Image.asset(
+                            'assets/images/logo.png',
+                            width: 184,
+                          ),
+                        ), //Nome do Aplicativo
+                        const Text(
+                          'CADASTRO',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        const SizedBox(height: 15),
+                        //Input do nome
+
+                        MyTextField(
+                          hintText: 'Nome',
+                          obscureText: false,
+                          controller: controllerUserName,
+                        ),
+
+                        const SizedBox(height: 10),
+                        //Input do email
+                        Container(
+                          width: 270,
+                          child: MyTextField(
+                            hintText: 'Telefone',
+                            obscureText: false,
+                            controller: controllerEmail,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        //Input da senha
+                        Container(
+                          width: 270,
+                          child: MyTextField(
+                            hintText: 'E-mail',
+                            obscureText: true,
+                            controller: controllerPass,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        //Input da confirme a senha
+                        Container(
+                          width: 270,
+                          child: MyTextField(
+                            hintText: 'Senha',
+                            obscureText: true,
+                            controller: controllerConfirmPass,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        //Botão de login
+                        MyButton(
+                          buttonText: 'SALVAR',
+                          onTapButton: () {
+                            displayMessage('Registrado', context);
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        //Possui uma conta? Faça login
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                goToLogin(context);
+                              },
+                              child: Text(
+                                'Possui uma conta? Faça login',
+                                style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
