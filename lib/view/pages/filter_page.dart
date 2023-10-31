55772d0c506545d74303ecfc5c478dd31c47dd81
Laurasouusa <laura.sousa@estudante.ifgoiano.edu.br>
2023-10-31 19:38:07 -0300

    filtro e 'meu perfil'

--- lib/view/pages/filter_page.dart
+++ lib/view/pages/filter_page.dart
@@ -1,15 +1,140 @@
 import 'package:flutter/material.dart';
 import 'package:melancia_express/view/components/my_appbar.dart';
+import 'package:melancia_express/view/components/my_bottombar.dart';
+import 'package:melancia_express/view/components/my_button.dart';
 
-class FilterPage extends StatelessWidget {
+class FilterPage extends StatefulWidget {
   FilterPage({super.key});
+
+  @override
+  _FilterPageState createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
+  List<String> selectedCategories = [];
+  List<String> selectedPrices = [];
+  List<String> selectedStatus = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: 'FILTRAR POR:'), // Usar o MyAppBar personalizado
-      body: Center(
-        child: Text('Conteúdo da página de filtrar'),
+      appBar: MyAppBar(title: 'FILTRAR POR:'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              FilterSection(
+                title: 'CATEGORIAS',
+                options: [
+                  'Sem semente',
+                  'Amarela',
+                  'Categoria 3',
+                  'Categoria 4'
+                ],
+                selectedOptions: selectedCategories,
+              ),
+              FilterSection(
+                title: 'PREÇOS',
+                options: ['1,00', '15,00', '2,50', '5,80'],
+                selectedOptions: selectedPrices,
+              ),
+              FilterSection(
+                title: 'STATUS',
+                options: ['Ativo', 'Inativo'],
+                selectedOptions: selectedStatus,
+              ),
+              const SizedBox(height: 20),
+              MyButton(
+                onTapButton: () {
+                  // Realizar a ação desejada com as opções selecionadas
+                  //print('Categorias selecionadas: $selectedCategories');
+                  // print('Preços selecionados: $selectedPrices');
+                  // print('Status selecionados: $selectedStatus');
+                },
+                buttonText: 'SALVAR',
+              ),
+            ],
+          ),
+        ),
       ),
+      bottomNavigationBar: MyBottomBar(),
+    );
+  }
+}
+
+class FilterSection extends StatefulWidget {
+  final String title;
+  final List<String> options;
+  final List<String> selectedOptions;
+
+  FilterSection({
+    required this.title,
+    required this.options,
+    required this.selectedOptions,
+  });
+
+  @override
+  _FilterSectionState createState() => _FilterSectionState();
+}
+
+class _FilterSectionState extends State<FilterSection> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 8, right: 8, bottom: 8, top: 8),
+          decoration: BoxDecoration(
+            border: Border.all(
+                color:
+                    Color.fromARGB(255, 197, 196, 196)), // Adiciona borda cinza
+            borderRadius: BorderRadius.circular(5), // Borda arredondada
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 231, 230, 230), // Cor do botão
+              ),
+            ],
+          ),
+          child: ListTile(
+            title: Text(
+              widget.title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            trailing:
+                Icon(isExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down),
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+          ),
+        ),
+        if (isExpanded)
+          Column(
+            children: widget.options.map((option) {
+              return ListTile(
+                title: Text(option),
+                leading: Checkbox(
+                  value: widget.selectedOptions.contains(option),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value != null) {
+                        if (value) {
+                          widget.selectedOptions.add(option);
+                        } else {
+                          widget.selectedOptions.remove(option);
+                        }
+                      }
+                    });
+                  },
+                ),
+              );
+            }).toList(),
+          ),
+      ],
     );
   }
 }
--- lib/view/pages/my_profile.dart
+++ lib/view/pages/my_profile.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:melancia_express/view/components/my_appbar.dart';
+import 'package:melancia_express/view/components/my_button.dart';
+import 'package:melancia_express/view/components/my_edittextfield.dart';
+import 'package:melancia_express/view/components/my_textfield.dart';
+import 'package:melancia_express/view/helpers/interface_helpers.dart';
 
+// ignore: must_be_immutable
 class MyPerfil extends StatelessWidget {
   MyPerfil({super.key});
+  TextEditingController controllerUserName = TextEditingController();
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerPass = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: 'Meu Perfil:'), // Usar o MyAppBar personalizado
+      appBar: MyAppBar(title: 'MEU PERFIL'),
       body: Center(
-        child: Text('Conteúdo da página de Perfil do usuario'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MyEditTextField(
+              hintText: 'Nome',
+              obscureText: false,
+              controller: controllerUserName,
+            ),
+            const SizedBox(height: 15),
+            MyEditTextField(
+              hintText: 'Telefone',
+              obscureText: false,
+              controller: controllerEmail,
+            ),
+            const SizedBox(height: 15),
+            MyEditTextField(
+              hintText: 'Email',
+              obscureText: false,
+              controller: controllerEmail,
+            ),
+            const SizedBox(height: 15),
+            MyEditTextField(
+              hintText: 'Senha',
+              obscureText: true,
+              controller: controllerPass,
+            ),
+            const SizedBox(height: 250),
+            MyButton(
+              buttonText: 'SALVAR',
+              onTapButton: () {
+                displayMessage('Registrado', context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
