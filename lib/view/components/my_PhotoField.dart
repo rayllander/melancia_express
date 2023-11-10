ed9267498fb0f4ba86108c076003f38fbfa48d44
mariaduardasa <maria.sa1@estudante.ifgoiano.edu.br>
2023-11-09 21:43:44 -0300

    alterações

--- lib/view/components/my_PhotoField.dart
+++ lib/view/components/my_PhotoField.dart
@@ -7,19 +7,30 @@ class PhotoField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(5), // Valor para a borda arredondada
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Color(0xFFBEBDBD), // Cor da borda semelhante ao TextField
+    return Padding(
+      padding: const EdgeInsets.all(0.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: onTap,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5), // Valor para a borda arredondada
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFFBEBDBD), // Cor da borda semelhante ao TextField
+                  ),
+                  color: Color(0xFFD9D9D9), // Cor de preenchimento
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(100.0), // Ajuste o valor do padding interno conforme desejado
+                  child: Icon(Icons.camera_alt, color: Colors.grey[600]),
+                ),
+              ),
             ),
-            color: Color(0xFFD9D9D9), // Cor de preenchimento
           ),
-          child: Icon(Icons.camera_alt, size: 60, color: Colors.grey[600]),
-        ),
+        ],
       ),
     );
   }
--- lib/view/pages/announcement_page.dart
+++ lib/view/pages/announcement_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:melancia_express/view/components/my_appbar.dart';
 import 'package:melancia_express/view/components/my_button.dart';
 import 'package:melancia_express/view/components/my_textfield.dart';
 import 'package:melancia_express/view/components/my_PhotoField.dart';
-import 'package:melancia_express/view/helpers/interface_helpers.dart';
-import 'package:melancia_express/view/components/my_appbar.dart';
+import 'package:melancia_express/view/helpers/interface_helpers.dart'; // Corrigido o nome do arquivo
 
 // ignore: must_be_immutable
 class AnnouncementPage extends StatelessWidget {
@@ -21,95 +21,68 @@ class AnnouncementPage extends StatelessWidget {
     return Scaffold(
       appBar: MyAppBar(title: 'CRIAR ANÚNCIO'),
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            MyTextField(
-                              hintText: 'Categoria',
-                              obscureText: false,
-                              controller: controllerCategory,
-                            ),
-                            SizedBox(height: 20),
-                            MyTextField(
-                              hintText: 'Data de colheita',
-                              obscureText: false,
-                              controller: controllerHarvestDate,
-                            ),
-                            SizedBox(height: 20),
-                            MyTextField(
-                              hintText: 'Status',
-                              obscureText: false,
-                              controller: controllerStatus,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            PhotoField(
-                              onTap: () {
-                                // Lógica para selecionar a imagem
-                              },
-                            ),
-                            SizedBox(height: 20),
-                            MyTextField(
-                              hintText: 'Valor',
-                              obscureText: false,
-                              controller: controllerValue,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: PhotoField(
+                  onTap: () {
+                    // Lógica para selecionar a imagem
+                  },
                 ),
-                SizedBox(height: 20),
-                MyTextField(
-                  hintText: 'Telefone',
-                  obscureText: false,
-                  controller: controllerTelephone,
+              ),
+              SizedBox(height: 20),
+              MyTextField(
+                hintText: 'Categoria',
+                obscureText: false,
+                controller: controllerCategory,
+              ),
+              SizedBox(height: 20),
+              MyTextField(
+                hintText: 'Data de colheita',
+                obscureText: false,
+                controller: controllerHarvestDate,
+              ),
+              SizedBox(height: 20),
+              MyTextField(
+                hintText: 'Status',
+                obscureText: false,
+                controller: controllerStatus,
+              ),
+              SizedBox(height: 20),
+              MyTextField(
+                hintText: 'Valor',
+                obscureText: false,
+                controller: controllerValue,
+              ),
+              SizedBox(height: 20),
+              MyTextField(
+                hintText: 'Telefone',
+                obscureText: false,
+                controller: controllerTelephone,
+              ),
+              SizedBox(height: 20),
+              MyTextField(
+                hintText: 'E-mail',
+                obscureText: true,
+                controller: controllerEmail,
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(60.0),
+                child: MyButton(
+                  buttonText: 'SALVAR',
+                  onTapButton: () {
+                    displayMessage('Registrado', context);
+                  },
                 ),
-                SizedBox(height: 20),
-                MyTextField(
-                  hintText: 'E-mail',
-                  obscureText: true,
-                  controller: controllerEmail,
-                ),
-                SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.all(60.0),
-                  child: MyButton(
-                    buttonText: 'SALVAR',
-                    onTapButton: () {
-                      displayMessage('Registrado', context);
-                    },
-                  ),
-                )
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
